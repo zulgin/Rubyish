f6d2e206abe455afcdab37879c7abd94aeeb4357
zulgin <zigster3@yahoo.com>
2014-03-11 20:36:46 -0700

    comment stakes

---
+++ Learn Ruby the Hard Way/exexc.rb
@@ -0,0 +1,61 @@
+# prints the string "I will now count my chickens" you fucking chicken knoikc knoic knoic. you..... you.... chicken... chicken... chicken knoic knoic knoic I need a reason to believe 151 aunque en volum bajo pero en buenos speakers bistec a los oidos.
+puts "I will now count my chickens"
+
+# prints the string "Hens", prints the result of the following operation:divides 30 into 6 and adds the quotient to 25 which is 30
+puts "Hens", 25 + 30 / 6
+
+# prints the string "Roosters", prints the result of the following operation:4% of 3 is multiplied by 25 the product of which is subtracted from 100 to equal 97
+# or... prints the result of the following operation: 25 is multiplied by 3, 4% of the product is then subtracted from 100 to equal 97
+puts "Roosters", 100 - 25 * 3 % 4
+
+# prints the string "Now I will count the eggs:"
+puts "Now I will count the eggs:"
+
+# prints the result of the following operation: 1 is divided into 4 the quotient of which is subtracted from 4% of 2. (151 28:50 this is how code is written) the sum? is added to 3 + 2 + 1 - 5 the sum of which 6 is added and equals 7... very chronological the computer does everything in a particular order does it not? linear? So hay que say it in the order it takes place like our brains? focus. aleman tambien. vamos.
+puts 3 + 2 +1 - 5 + 4 % 2 - 1 / 4 + 6
+
+# hmm a true/false inquary is made as to the validity of the statement 3 + 2 < 5 -7 and the  inquary response "false" is printed
+puts 3 + 2 < 5 - 7
+
+# prints the string "What is 3 + 2?" and prints the result of the following operation 3 + 2 which is 5
+puts "What is 3 + 2?", 3 + 2
+
+# prints the string "What is 5 - 7?" and prints the result of 5 - 7 which is -2
+puts "What is 5 - 7?", 5 - 7
+
+# prints the string "Oh, that's why it's false."
+puts "Oh, that's why it's false."
+
+# prints the string "How about some more."
+puts "How about some more."
+
+# prints the string "Is it greater?" and makes an true/false inquary operation as to the validity of the statement "5 > -2" and prints the response "true"
+puts "Is it greater?", 5 > -2
+
+# prints the string "Is it greater or equal?" and makes a true/false inquary as to the validity of the statement "5 >= -2" and prints the response "true"
+puts "Is it greater or equal?", 5 >= -2
+
+# prints the string "Is it less or equal?" and makes a true/false inquary as to the validity of the statement "5 <= -2" and prints the response "false"
+puts "Is it less or equal?", 5 <= -2
+
+puts 3 + 2
+puts 3 + 6
+puts 4>2
+puts 4 >2
+puts 4 > 2
+puts 4 >  2
+puts 4   >    2
+puts 4     >    2
+puts 4    >    2
+puts 4   > 2
+puts 4     > 3
+puts 4    > 8
+puts 4 > 4
+puts 4 > 0
+puts 4 > -2
+puts 4 > 6
+puts 4/5
+puts 4/4
+puts 4 <=4
+puts 4  * 4
+puts 4 % 4
